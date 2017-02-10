bb01bb05495b537fc89733e4a5d15ef5acaaf4d0
Raeed Hasan Chowdhury <raeed.chowdhury@gmail.com>
2017-02-09 20:47:23 -0600

    add confidence intervals to PEH

--- lib/getPEH.m
+++ lib/getPEH.m
@@ -1,12 +1,16 @@
-function [h,averageData,dataCIlow,dataCIhigh] = getPEH(dataTimes,data,eventTimes,window)
+function [averageData,dataCIlow,dataCIhigh] = getPEH(dataTimes,data,eventTimes,window)
 
 % extract relevant times
-for i = 1:length(eventTimes)
+numEvents = length(eventTimes);
+for i = 1:numEvents
     tmp = data(dataTimes>=roundTime(eventTimes(i)+window(1),0.0005) & dataTimes<roundTime(eventTimes(i)+window(2),0.0005))';
     dataWindow(i,:) = tmp;
 end
 
-h=0;
 averageData = mean(dataWindow);
-dataCIlow = 0;
-dataCIhigh = 0;
+
+binned_stderr = std(dataWindow)/sqrt(numEvents); % standard error
+tscore = tinv(0.975,numEvents-1); % t-score for 95% CI
+
+dataCIlow = averageData - binned_stderr*tscore;
+dataCIhigh = averageData + binned_stderr*tscore;
