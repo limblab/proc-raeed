cf8b62a7848b9cb02ab92755db8db6003461b3b6
Raeed Hasan Chowdhury <raeed.chowdhury@gmail.com>
2017-02-11 17:24:51 -0600

    finished (minus bugchecking) model conversion script

--- Hindlimb/leg_lib/Neuromechanic/convert_model.m
+++ Hindlimb/leg_lib/Neuromechanic/convert_model.m
@@ -1,3 +1,5 @@
+function legmodel = convert_model(plotflag)
+
 %% find flexion axes for 2D model
 % load ncmb
 nmcb = read_nmcb('basicCatHindlimb_201201.nmcb');
@@ -111,14 +113,16 @@ end
 muscles = [muscles cell2table(oiv_world,'VariableNames',{'oiv_world'})];
 
 %% plot muscle points to check
-h=plot3(worldpoint(1,:,1), worldpoint(2,:,1), worldpoint(3,:,1), 'k-','linewidth',3);
-hold on
-h=plot3(worldpoint(1,:,1), worldpoint(2,:,1), worldpoint(3,:,1), 'bo','markersize',10,'linewidth',2);
-for musc_idx = 1:height(muscles)
-    oiv_world = muscles.oiv_world{musc_idx};
-    plot3(oiv_world(:,1),oiv_world(:,2),oiv_world(:,3),'r.-','linewidth',2)
+if(plotflag)
+    h=plot3(worldpoint(1,:,1), worldpoint(2,:,1), worldpoint(3,:,1), 'k-','linewidth',3);
+    hold on
+    h=plot3(worldpoint(1,:,1), worldpoint(2,:,1), worldpoint(3,:,1), 'bo','markersize',10,'linewidth',2);
+    for musc_idx = 1:height(muscles)
+        oiv_world = muscles.oiv_world{musc_idx};
+        plot3(oiv_world(:,1),oiv_world(:,2),oiv_world(:,3),'r.-','linewidth',2)
+    end
+    axis equal
 end
-axis equal
 
 %% Find world point corresponding to flexion axes 
 
@@ -132,8 +136,7 @@ ankle_flex = interp1(ankle_points(3,:)',ankle_points(1:2,:)',zplane,'linear','ex
 
 % pelvis
 pelvis2d_axes = eye(2);
-plot3([0 pelvis2d_axes(1,1)]/50 + pelvis_origin(1),[0 pelvis2d_axes(2,1)]/50 + pelvis_origin(2),[zplane zplane],'g','linewidth',2)
-plot3([0 pelvis2d_axes(1,2)]/50 + pelvis_origin(1),[0 pelvis2d_axes(2,2)]/50 + pelvis_origin(2),[zplane zplane],'g','linewidth',2)
+pelvis2d_origin = pelvis_origin(1:2);
 
 % femur - hip rotation center (worldpoint 4) to new knee flexion axis
 femur2d_x = knee_flex-worldpoint(1:2,4);
@@ -141,8 +144,7 @@ femur2d_x = femur2d_x/norm(femur2d_x);
 femur2d_y = cross([0;0;1],[femur2d_x;0]);
 femur2d_y = femur2d_y(1:2);
 femur2d_axes = [femur2d_x femur2d_y];
-plot3([0 femur2d_axes(1,1)]/50 + femur_origin(1),[0 femur2d_axes(2,1)]/50 + femur_origin(2),[zplane zplane],'g','linewidth',2)
-plot3([0 femur2d_axes(1,2)]/50 + femur_origin(1),[0 femur2d_axes(2,2)]/50 + femur_origin(2),[zplane zplane],'g','linewidth',2)
+femur2d_origin = femur_origin(1:2);
 
 % tibia - new knee flexion axis to new ankle flexion axis
 tibia2d_x = ankle_flex-knee_flex;
@@ -150,8 +152,7 @@ tibia2d_x = tibia2d_x/norm(tibia2d_x);
 tibia2d_y = cross([0;0;1],[tibia2d_x;0]);
 tibia2d_y = tibia2d_y(1:2);
 tibia2d_axes = [tibia2d_x tibia2d_y];
-plot3([0 tibia2d_axes(1,1)]/50 + tibia_origin(1),[0 tibia2d_axes(2,1)]/50 + tibia_origin(2),[zplane zplane],'g','linewidth',2)
-plot3([0 tibia2d_axes(1,2)]/50 + tibia_origin(1),[0 tibia2d_axes(2,2)]/50 + tibia_origin(2),[zplane zplane],'g','linewidth',2)
+tibia2d_origin = knee_flex;
 
 % foot - new ankle flexion axis to toe (world point 11)
 foot2d_x = worldpoint(1:2,11)-ankle_flex;
@@ -159,5 +160,65 @@ foot2d_x = foot2d_x/norm(foot2d_x);
 foot2d_y = cross([0;0;1],[foot2d_x;0]);
 foot2d_y = foot2d_y(1:2);
 foot2d_axes = [foot2d_x foot2d_y];
-plot3([0 foot2d_axes(1,1)]/50 + foot_origin(1),[0 foot2d_axes(2,1)]/50 + foot_origin(2),[zplane zplane],'g','linewidth',2)
-plot3([0 foot2d_axes(1,2)]/50 + foot_origin(1),[0 foot2d_axes(2,2)]/50 + foot_origin(2),[zplane zplane],'g','linewidth',2)
+foot2d_origin = ankle_flex;
+
+if(plotflag)
+    plot3([0 pelvis2d_axes(1,1)]/50 + pelvis2d_origin(1),[0 pelvis2d_axes(2,1)]/50 + pelvis2d_origin(2),[zplane zplane],'g','linewidth',2)
+    plot3([0 pelvis2d_axes(1,2)]/50 + pelvis2d_origin(1),[0 pelvis2d_axes(2,2)]/50 + pelvis2d_origin(2),[zplane zplane],'g','linewidth',2)
+    plot3([0 femur2d_axes(1,1)]/50 + femur2d_origin(1),[0 femur2d_axes(2,1)]/50 + femur2d_origin(2),[zplane zplane],'g','linewidth',2)
+    plot3([0 femur2d_axes(1,2)]/50 + femur2d_origin(1),[0 femur2d_axes(2,2)]/50 + femur2d_origin(2),[zplane zplane],'g','linewidth',2)
+    plot3([0 tibia2d_axes(1,1)]/50 + tibia2d_origin(1),[0 tibia2d_axes(2,1)]/50 + tibia2d_origin(2),[zplane zplane],'g','linewidth',2)
+    plot3([0 tibia2d_axes(1,2)]/50 + tibia2d_origin(1),[0 tibia2d_axes(2,2)]/50 + tibia2d_origin(2),[zplane zplane],'g','linewidth',2)
+    plot3([0 foot2d_axes(1,1)]/50 + foot2d_origin(1),[0 foot2d_axes(2,1)]/50 + foot2d_origin(2),[zplane zplane],'g','linewidth',2)
+    plot3([0 foot2d_axes(1,2)]/50 + foot2d_origin(1),[0 foot2d_axes(2,2)]/50 + foot2d_origin(2),[zplane zplane],'g','linewidth',2)
+end
+
+%% make new muscle struct with 2D oiv points in segment coordinates
+
+% copy muscles struct
+muscles_2d = muscles;
+
+% prep oiv2d cell
+oiv_2d= cell(height(muscles),1);
+
+for musc_idx = 1:height(muscles)
+    oiv = muscles.oiv_world{musc_idx};
+    for oiv_idx = 1:size(oiv,1)
+        if strcmp(oivsegment{oiv_idx},'pelvis')
+            oiv_2d{musc_idx}(oiv_idx,:) = (oiv(oiv_idx,:)-[pelvis2d_origin' zplane])*[pelvis2d_axes;0 0];
+        elseif strcmp(oivsegment{oiv_idx},'femur')
+            oiv_2d{musc_idx}(oiv_idx,:) = (oiv(oiv_idx,:)-[femur2d_origin' zplane])*[femur2d_axes;0 0];
+        elseif strcmp(oivsegment{oiv_idx},'tibia')
+            oiv_2d{musc_idx}(oiv_idx,:) = (oiv(oiv_idx,:)-[tibia2d_origin' zplane])*[tibia2d_axes;0 0];
+        elseif strcmp(oivsegment{oiv_idx},'foot')
+            oiv_2d{musc_idx}(oiv_idx,:) = (oiv(oiv_idx,:)-[foot2d_origin' zplane])*[foot2d_axes;0 0];
+        else
+            error('wrong frame')
+        end
+    end
+end
+
+muscles_2d.oiv = oiv_2d;
+
+%% get locations of bod(i+1) origin relative to bod(i) in 2d coordinates
+
+% Bods = {pelvis,femur,tibia,foot}
+% origins = {ground,hip center, knee flexion, ankle flexion}
+bodloc_2d = zeros(2,4); %location of bod(i+1) relative to bod(i) (location of bod(1) = 0)
+
+bodloc_2d(:,2) = pelvis2d_axes'*(femur2d_origin-pelvis2d_origin);
+bodloc_2d(:,3) = femur2d_axes'*(tibia2d_origin-femur2d_origin);
+bodloc_2d(:,4) = tibia2d_axes'*(foot2d_origin-tibia2d_origin);
+
+%% find default angles for the 2d model
+
+hip_ang = atan2d(femur2d_axes(2,1),femur2d_axes(1,1))-atan2d(pelvis2d_axes(2,1),pelvis2d_axes(1,1));
+knee_ang = atan2d(tibia2d_axes(2,1),tibia2d_axes(1,1))-atan2d(femur2d_axes(2,1),femur2d_axes(1,1));
+ankle_ang = atan2d(foot2d_axes(2,1),foot2d_axes(1,1))-atan2d(tibia2d_axes(2,1),tibia2d_axes(1,1));
+
+%% Save 2d model
+
+legmodel.bodloc = bodloc_2d;
+legmodel.muscles = muscles;
+legmodel.hipcenter = bodloc_2d(:,2);
+legmodel.default_angles = [hip_ang;knee_ang;ankle_ang];
--- Hindlimb/leg_lib/Neuromechanic/world2segment.m
+++
@@ -1,2 +0,0 @@
-function world2segment(nmcb,worldpoint,segment,q)
-% Gives the world point in segment coordinates for a given configuration
