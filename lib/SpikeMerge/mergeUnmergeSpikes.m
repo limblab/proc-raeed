ae6ee24c3c7b1fa55e1f7acdf3a1b8cdb63fa8d3
Raeed Hasan Chowdhury <raeed.chowdhury@gmail.com>
2017-02-20 16:41:23 -0600

    Delete some unneeded files

--- lib/SpikeMerge/mergeUnmergeSpikes.m
+++ lib/SpikeMerge/mergeUnmergeSpikes.m
@@ -7,8 +7,8 @@
 % sure that you create different bdfs for each task (unless you know
 % what you're doing.)
 
-file_prefix_all = 'Han_20170208';
-file_path = 'C:\Users\rhc307\Projects\limblab\data-raeed\BumpCurl\Han\20170208\preCDS\merging\';
+file_prefix_all = 'Han_20170203';
+file_path = 'C:\Users\rhc307\Projects\limblab\data-raeed\ForceKin\Han\20170203\preCDS\merging\';
 % file_path = 'D:\Chewie_8I2\Chewie_2015-05-05_DCO_emg_emg_cartesian\CerebusData\';
 % file_prefix_all = 'Chewie_2015-05-05_DCO_emg_emg_cartesian_';
 % file_path = 'D:\Data\Mini_7H1\';
