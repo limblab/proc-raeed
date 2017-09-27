67ea3b9d547a12660381b95dcabc103f0ae449d3
Raeed Hasan Chowdhury <raeed.chowdhury@gmail.com>
2017-09-27 15:22:01 -0500

    add example of how to run color_tracking script in loadsave_cds.m

--- loadsave_cds.m
+++ loadsave_cds.m
@@ -1,18 +1,52 @@
+%% Load colorTracking file
+main_dir='C:\Users\rhc307\Projects\limblab\data-preproc\ForceKin\Chips\20170913';
+monkey='Chips';
+date='20170913'; %mo-day-yr
+exp='COactpas';
+num='001';
+% file_name = 'Han_20170206_CObumpcurl_adaptation_colorTracking_003';
+file_name = [monkey '_' date '_' exp '_colorTracking_' num];
+
+% Load ColorTracking File and Settings
+fname_load=ls([main_dir '/ColorTracking/' file_name '*']);
+load(deblank([main_dir '/ColorTracking/' fname_load]))
+
+%% Run color tracking script
+color_tracker_4colors_script;
+
+%% Save
+if savefile
+    fname_save=[main_dir '\ColorTracking\Markers\markers_' monkey '_' date '_' exp '_' num];
+    save(fname_save,'all_medians','all_medians2','led_vals','times');
+    
+    if first_time
+        fname_save_settings=[main_dir '\ColorTracking\Markers\settings_' monkey '_' date];
+        save(fname_save_settings,'red_elbow_dist_from_blue','red_blue_arm_dist_max',...
+            'green_hand_dists_elbow','red_hand_dists_elbow','blue_hand_dists_elbow','yellow_hand_dists_elbow','green_separator',...
+            'green_hand_dists_bluearm','red_hand_dists_bluearm','blue_hand_dists_bluearm','yellow_hand_dists_bluearm',...
+            'green_hand_dists_redarm', 'red_hand_dists_redarm', 'blue_hand_dists_redarm','yellow_hand_dists_redarm',...
+            'green_dist_min','red_keep','green_keep','blue_keep','yellow_keep','marker_inits');
+    end
+end
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear
 %% Load data into CDS file
 
 meta.lab=6;
 meta.ranBy='ranByRaeed';
-meta.monkey='monkeyHan';
-meta.task='taskRW';
+meta.monkey='monkeyChips';
+meta.task='taskCObump';
 meta.array='arrayLeftS1Area2';
-meta.folder='C:\Users\rhc307\Projects\limblab\data-preproc\MultiWorkspace\SplitWS\Han\20160322\';
-meta.fname='Han_20160322_RW_PM_area2_002';
-meta.mapfile='mapFileC:\Users\rhc307\Projects\limblab\data-preproc\Meta\Mapfiles\Han\left_S1\SN 6251-001459.cmp';
+meta.folder='C:\Users\rhc307\Projects\limblab\data-preproc\ForceKin\Chips\20170913\';
+meta.fname='Chips_20170913_COactpas_area2_001';
+meta.mapfile='mapFileC:\Users\rhc307\Projects\limblab\data-preproc\Meta\Mapfiles\Chips\left_S1\SN 6251-001455.cmp';
 
 % Make CDS files
 
 cds = commonDataStructure();
 cds.file2cds([meta.folder 'preCDS\' meta.fname],meta.ranBy,meta.array,meta.monkey,meta.lab,'ignoreJumps',meta.task,meta.mapfile);
+% also load second EMG file if necessary
 
 %% Load marker file
 
@@ -34,6 +68,9 @@ cds.loadOpenSimData([meta.folder 'OpenSim\Analysis\'],'joint_ang')
 
 % load joint velocities
 cds.loadOpenSimData([meta.folder 'OpenSim\Analysis\'],'joint_vel')
+
+% load joint moments
+cds.loadOpenSimData([meta.folder 'OpenSim\Analysis\'],'joint_dyn')
 
 % load muscle information
 cds.loadOpenSimData([meta.folder 'OpenSim\Analysis\'],'muscle_len')
