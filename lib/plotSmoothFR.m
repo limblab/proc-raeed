8a56bb3e85e0e9e1503d440dc368092ef5bb62a1
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2018-03-23 18:54:14 -0500

    add tuning weight cloud and tuning surface plotting code to lib and multiworkspace

--- lib/plotSmoothFR.m
+++ lib/plotSmoothFR.m
@@ -5,12 +5,18 @@ if size(fr,2)>1
     error('Too many firing rates, only one at a time')
 end
 
+x_cent = x-mean(x);
+y_cent = y-mean(y);
+if ~exist('gridrange','var')
+    gridrange = [min(min([x_cent,y_cent])) max(max([x_cent,y_cent]))];
+end
+
 [xq,yq] = meshgrid(linspace(gridrange(1),gridrange(2),100));
 
-fr_grid = griddata(x,y,fr,xq,yq,'v4');
-% scatter(xq(:),yq(:),10,fr_grid(:),'filled')
-% surf(xq,yq,fr_grid)
-imagesc(fr_grid)
+fr_grid = griddata(x_cent,y_cent,fr,xq,yq,'natural');
+scatter(xq(:)+mean(x),yq(:)+mean(y),10,fr_grid(:),'filled')
+% mesh(xq,yq,fr_grid)
+% imagesc(fr_grid)
 
 clims = prctile(fr,[5 95]);
 caxis(clims)
